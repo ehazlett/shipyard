2c100e060c6082ec901e85395edfa91bc1ad9a79
Evan Hazlett <ejhazlett@gmail.com>
2013-07-14 01:44:40 -0400

    more test setup

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 VE_DIR=./.ve
+if [ -e "/etc/lsb-release" ]; then
+    apt-get install -y python-setuptools python-dev
+fi
 easy_install virtualenv
 virtualenv --no-site-packages $VE_DIR
 $VE_DIR/bin/pip install -r requirements.txt
