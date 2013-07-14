a8c403b7967140b9545dd307c83e6d269fec0c0a
Evan Hazlett <ejhazlett@gmail.com>
2013-07-14 02:00:25 -0400

    updated test script

--- test.sh
+++ test.sh
@@ -7,5 +7,3 @@ easy_install virtualenv
 virtualenv --no-site-packages $VE_DIR
 $VE_DIR/bin/pip install -r requirements.txt
 python manage.py test accounts applications containers dashboard shipyard
-rm -rf $VE_DIR
-
