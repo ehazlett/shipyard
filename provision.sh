3ce86d9bca8dbf55034064fa272d023bdaa72fe8
Evan Hazlett <ejhazlett@gmail.com>
2013-08-22 20:37:00 -0400

    fixed provisioning for new docker build process

--- provision.sh
+++ provision.sh
@@ -7,7 +7,13 @@ NODE_URL='http://nodejs.org/dist/v0.10.12/node-v0.10.12.tar.gz'
 GIT_RECEIVER_URL='https://raw.github.com/ehazlett/gitreceive/master/gitreceive'
 
 apt-get -qq update
-DEBCONF_FRONTEND=noninteractive apt-get -qq install -y python-software-properties s3cmd git-core linux-image-extra-`uname -r` bridge-utils bsdtar lxc wget ruby python-dev libxml2-dev python-setuptools redis-server supervisor
+DEBIAN_FRONTEND=noninteractive apt-get -qq install -y python-software-properties s3cmd git-core linux-image-extra-`uname -r` bridge-utils bsdtar lxc wget ruby python-dev libxml2-dev python-setuptools redis-server supervisor
+add-apt-repository -y ppa:dotcloud/lxc-docker
+apt-get -qq update
+DEBIAN_FRONTEND=noninteractive apt-get install -y lxc-docker
+# edit docker upstart to listen on tcp
+sed -i 's/\/usr\/bin\/docker.*/\/usr\/bin\/docker -d -H tcp:\/\/0.0.0.0:4243/g' /etc/init/docker.conf
+service docker restart
 
 mkdir -p $VE_ROOT
 
@@ -38,19 +44,6 @@ cat << EOF > /etc/hipache.config.json
     "redisPort": 6379
 }
 EOF
-
-# install go & docker
-if [ ! -e "/usr/local/bin/docker" ] ; then
-    cd /tmp
-    wget https://go.googlecode.com/files/go1.1.1.linux-amd64.tar.gz -O go.tar.gz
-    cd /opt ; tar zxf /tmp/go.tar.gz
-    grep -v go /etc/environment && echo \'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/opt/go/bin"\' > /etc/environment
-    PATH=$PATH:/opt/go/bin
-    if [ -e docker ]; then rm -rf docker; fi ; git clone https://github.com/dotcloud/docker
-    cd docker ; git checkout master; GOROOT=/opt/go make
-    killall docker
-    cp -f bin/docker /usr/local/bin/docker
-fi
 # install gitreceiver
 cd /tmp
 wget $GIT_RECEIVER_URL -O /usr/local/bin/gitreceive --no-check-certificate
@@ -72,7 +65,7 @@ if [ "`grep \"source /opt/ve\" $VAGRANT_BASHRC`" = "" ]; then
     echo "source $VE_DIR/bin/activate" >> $VAGRANT_BASHRC
     echo "alias pm='python manage.py \$*'" >> $VAGRANT_BASHRC
     echo "alias pmr='python manage.py runserver 0.0.0.0:8000\$*'" >> $VAGRANT_BASHRC
-    echo "sudo /etc/init.d/supervisor restart" >> $VAGRANT_BASHRC
+    echo "alias pmq='python manage.py rqworker shipyard\$*'" >> $VAGRANT_BASHRC
     echo "echo ''" >> $VAGRANT_BASHRC
     echo "cd $APP_DIR" >> $VAGRANT_BASHRC
 fi
@@ -81,13 +74,6 @@ cat << EOF > /etc/supervisor/conf.d/shipyard.conf
 [program:hipache]
 directory=/tmp
 command=hipache -c /etc/hipache.config.json
-user=root
-autostart=true
-autorestart=true
-
-[program:docker]
-directory=/tmp
-command=/usr/local/bin/docker -r -d -H 0.0.0.0:4243
 user=root
 autostart=true
 autorestart=true
