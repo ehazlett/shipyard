7809556852312d80626a1adbdc14827bea7b8d5e
Evan Hazlett <ejhazlett@gmail.com>
2013-10-06 11:03:10 -0400

    added tests for api (hosts, containers)

--- containers/tests.py
+++ containers/tests.py
@@ -1,16 +1,46 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from tastypie.test import ResourceTestCase
+from django.contrib.auth.models import User
 
-Replace this with more appropriate tests for your application.
-"""
+class ContainerResourceTest(ResourceTestCase):
+    fixtures = ['test_containers.json']
 
-from django.test import TestCase
+    def setUp(self):
+        super(ContainerResourceTest, self).setUp()
+        self.api_list_url = '/api/v1/containers/'
+        self.username = 'testuser'
+        self.password = 'testpass'
+        self.user = User.objects.create_user(self.username,
+            'testuser@example.com', self.password)
+        self.api_key = self.user.api_key.key
 
+    def get_credentials(self):
+        return self.create_apikey(self.username, self.api_key)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def test_get_list_unauthorzied(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test get without key returns unauthorized
         """
-        self.assertEqual(1 + 1, 2)
+        self.assertHttpUnauthorized(self.api_client.get(self.api_list_url,
+            format='json'))
+
+    def test_get_list_json(self):
+        """
+        Test get application list
+        """
+        resp = self.api_client.get(self.api_list_url, format='json',
+            authentication=self.get_credentials())
+        self.assertValidJSONResponse(resp)
+
+    def test_get_detail_json(self):
+        """
+        Test get application details
+        """
+        from containers.models import Container
+        url = '{}1/'.format(self.api_list_url)
+        resp = self.api_client.get(url, format='json',
+            authentication=self.get_credentials())
+        self.assertValidJSONResponse(resp)
+        data = self.deserialize(resp)
+        keys = data.keys()
+        self.assertTrue('container_id' in keys)
+
