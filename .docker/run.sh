82793000d706bec24879628e8673a8104139fdf3
Evan Hazlett <ejhazlett@gmail.com>
2013-10-27 11:34:03 -0400

    fixes #88: moved nginx.conf generation to run.sh to configure redis, resolver, etc.

--- .docker/run.sh
+++ .docker/run.sh
@@ -75,11 +75,84 @@ if [ -z "$SKIP_DEPLOY" ] ; then
     git pull --ff-only origin $REVISION
 fi
 # nginx resolver
-if [ ! -z "$NGINX_RESOLVER" ] ; then
-    sed -i "s/resolver.*/resolver $NGINX_RESOLVER;/g" $APP_DIR/.docker/nginx.conf
+cat << EOF >> $APP_DIR/.docker/nginx.conf
+daemon off;
+worker_processes  1;
+access_log $LOG_DIR/nginx_access.log
+error_log $LOG_DIR/nginx_error.log;
 
+events {
+  worker_connections 1024;
+}
+
+http {
+  server {
+    listen 8000;
+
+    location / {
+      proxy_pass http://127.0.0.1:8001;
+      proxy_set_header Host \$http_host;
+      proxy_set_header X-Forwarded-Host \$host;
+      proxy_set_header X-Real-IP \$remote_addr;
+      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+
+    location /console/ {
+      resolver $NGINX_RESOLVER;
+
+      set \$target '';
+      rewrite_by_lua '
+        local session_id = " "
+        local match, err = ngx.re.match(ngx.var.uri, "(/console/(?<id>.*)/)")
+        if match then
+            session_id = match["id"]
+        else
+            if err then
+                ngx.log(ngx.ERR, "error: ", err)
+                return
+            end
+            ngx.say("url malformed")
+        end
+
+        local key = "console:" .. session_id
+
+        local redis = require "resty.redis"
+        local red = redis:new()
+
+        red:set_timeout(1000) -- 1 second
+
+        local ok, err = red:connect($REDIS_HOST, $REDIS_PORT)
+        if not ok then
+            ngx.log(ngx.ERR, "failed to connect to redis: ", err)
+            return ngx.exit(500)
+        end
+
+        local console, err = red:hmget(key, "host", "path")
+        if not console then
+            ngx.log(ngx.ERR, "failed to get redis key: ", err)
+            return ngx.exit(500)
+        end
+
+        if console == ngx.null then
+            ngx.log(ngx.ERR, "no console session found for key ", key)
+            return ngx.exit(400)
+        end
+
+        ngx.var.target = console[1]
+        
+        ngx.req.set_uri(console[2])
+      ';
+ 
+      
+      proxy_pass http://\$target;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade \$http_upgrade;
+      proxy_set_header Connection "upgrade";
+      proxy_read_timeout 7200s;
+    }
+  }
+}
 EOF
-fi
 if [ ! -z "$EXTRA_CMD" ]; then
     /bin/bash -c "$EXTRA_CMD"
 fi
