d911a5557e7fa2974bf756d78491414fa71c6ce3
Evan Hazlett <ejhazlett@gmail.com>
2013-12-16 08:31:00 -0500

    updated allowed_hosts in settings

--- shipyard/settings.py
+++ shipyard/settings.py
@@ -57,7 +57,10 @@ REDIS_DB = os.getenv('REDIS_DB', 0)
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', None)
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+
+# NOTE: this is a wildcard to help with deployments ; please make sure
+# to update to your own if you wish to tighten security
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
