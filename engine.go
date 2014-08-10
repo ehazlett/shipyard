e8068694f98270bfb074acce846a4bf80ea005ca
Evan Hazlett <ejhazlett@gmail.com>
2014-08-10 14:26:56 -0400

    renamed engine json struct tags

--- engine.go
+++ engine.go
@@ -7,9 +7,9 @@ import (
 type (
 	Engine struct {
 		ID             string          `json:"id,omitempty" gorethink:"id,omitempty"`
-		SSLCertificate string          `json:"ssl-cert,omitempty" gorethink:"ssl-cert,omitempty"`
-		SSLKey         string          `json:"ssl-key,omitempty" gorethink:"ssl-key,omitempty"`
-		CACertificate  string          `json:"ca-cert,omitempty" gorethink:"ca-cert,omitempty"`
+		SSLCertificate string          `json:"ssl_cert,omitempty" gorethink:"ssl-cert,omitempty"`
+		SSLKey         string          `json:"ssl_key,omitempty" gorethink:"ssl-key,omitempty"`
+		CACertificate  string          `json:"ca_cert,omitempty" gorethink:"ca-cert,omitempty"`
 		Engine         *citadel.Engine `json:"engine,omitempty" gorethink:"engine,omitempty"`
 	}
 )
