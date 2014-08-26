657337fb9c709843d9d589d251f850cd5d54a4c9
Evan Hazlett <ejhazlett@gmail.com>
2014-08-26 18:13:29 -0400

    use filter for different event types

--- controller/static/app/filters.js
+++ controller/static/app/filters.js
@@ -8,4 +8,24 @@ angular.module('shipyard.filters', [])
             }
             return t.substring(0, 12);
         };
+    })
+    .filter('eventCssClass', function () {
+        return function (t) {
+            var cls = "";
+            switch(t) {
+                case 'die':
+                    cls = "off red";
+                    break;
+                case 'start':
+                    cls = "checkmark green";
+                    break;
+                case 'create':
+                    cls = "add blue";
+                    break;
+                case 'restart':
+                    cls = "refresh blue";
+                    break;
+            }
+            return cls;
+        };
     });
