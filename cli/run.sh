741a3c6bd1d4de3cf44e03fd67dcc461cb067b02
Evan Hazlett <ejhazlett@gmail.com>
2014-09-04 02:50:04 -0400

    minor update to cli wrapper allowing named envs

--- cli/run.sh
+++ cli/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 blue='\e[0;34m'
 reset='\e[0m'
+if [ ! -z "$NAME" ]; then
+    NAME="-$NAME"
+fi
 function run() {
-    echo -ne "${blue}shipyard> ${reset}"
+    echo -ne "${blue}shipyard$NAME> ${reset}"
     read CMD
     shipyard-cli $CMD
 }
@@ -14,7 +17,7 @@ function kill() {
     if [ "$QUIT" = "y" ]; then
         exit 0
     else
-        echo -ne "${blue}shipyard> ${reset}"
+        echo -ne "${blue}shipyard$NAME> ${reset}"
     fi
 }
 
