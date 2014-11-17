ac5522e49c32e8cea810b18c51e3bb797bddd2f3
Tom Barlow <tomwbarlow@gmail.com>
2014-11-17 03:15:54 -0500

    Moved EnginesController into its own module

--- controller/static/app/controllers.js
+++ controller/static/app/controllers.js
@@ -428,36 +428,6 @@ angular.module('shipyard.controllers', ['ngCookies'])
                 $scope.container = data;
             });
         })
-        .controller('EnginesController', function($scope, $location, Engines) {
-            $scope.orderByField = 'engine.id';
-            $scope.reverseSort = false;
-
-            $scope.go = function() {
-                $location.path("/engines/" + this.e.id)
-            };
-
-            $scope.selectSortColumn = function(field) {
-                $scope.reverseSort = !$scope.reverseSort;
-                $scope.orderByField = field;
-            }
-
-            $scope.sortedTableHeading = function(field) {
-                if($scope.orderByField != field) {
-                    return "";
-                } else {
-                    if($scope.reverseSort == true) {
-                        return "descending";
-                    } else {
-                        return "ascending";
-                    }
-                }
-            }
-            
-            $scope.template = 'templates/engines.html';
-            Engines.query(function(data){
-                $scope.engines = data;
-            });
-        })
         .controller('EngineAddController', function($scope, $location, $routeParams, flash, Engines) {
             $scope.template = 'templates/engine_add.html';
             $scope.id = "";
---
+++ controller/static/app/engines/engines.controller.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('shipyard')
+        .controller('EnginesController', function($scope, $location, Engines) {
+            $scope.orderByField = 'engine.id';
+            $scope.reverseSort = false;
+
+            $scope.go = function() {
+                $location.path("/engines/" + this.e.id)
+            };
+
+            $scope.selectSortColumn = function(field) {
+                $scope.reverseSort = !$scope.reverseSort;
+                $scope.orderByField = field;
+            }
+
+            $scope.sortedTableHeading = function(field) {
+                if($scope.orderByField != field) {
+                    return "";
+                } else {
+                    if($scope.reverseSort == true) {
+                        return "descending"
+                    } else {
+                        return "ascending";
+                    }
+                }
+            }
+            
+            $scope.template = 'templates/engines.html';
+            Engines.query(function(data){
+                $scope.engines = data;
+            });
+        });
+})();
