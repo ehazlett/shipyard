3606836abd945d97be3aa21f10d7c43f432202ba
Tom Barlow <tomwbarlow@gmail.com>
2015-02-02 08:16:20 -0500

    Dashboard operations

--- controller/static/app/containers/containers.controller.js
+++ controller/static/app/containers/containers.controller.js
@@ -5,15 +5,77 @@
         .module('shipyard.containers')
         .controller('ContainersController', ContainersController);
 
-    ContainersController.$inject = ['$location', 'resolveContainers', 'tablesort'];
+    ContainersController.$inject = ['$location', 'resolveContainers', 'Container', 'tablesort', 'flash', '$route', '$scope'];
 
-    function ContainersController($location, resolveContainers, tablesort) {
+    function ContainersController($location, resolveContainers, Container, tablesort, flash, $route, $scope) {
         var vm = this;
         vm.tablesort = tablesort;
         vm.containers = resolveContainers;
+        vm.activeContainer = null;
 
-        vm.go = function(container) {
-            $location.path("/containers/" + container.id);
+        vm.showDestroyContainerDialog = function(container) {
+            vm.activeContainer = container;
+            $('.basic.modal.removeContainer')
+                .modal('show');
+        };
+        vm.showStopContainerDialog = function(container) {
+            vm.activeContainer = container;
+            $('.basic.modal.stopContainer')
+                .modal('show');
+        };
+        vm.showRestartContainerDialog = function(container) {
+            vm.activeContainer = container;
+            $('.basic.modal.restartContainer')
+                .modal('show');
+        };
+
+        vm.showScaleContainerDialog = function(container) {
+            vm.activeContainer = container;
+            $('.basic.modal.scaleContainer')
+                .modal('show');
+        };
+
+        vm.destroyContainer = function(container) {
+            Container.destroy({id: container.id}).$promise.then(function() {
+                $route.reload();
+            }, function(err) {
+                flash.error = 'error destroying container: ' + err.data;
+            });
+        };
+
+        vm.stopContainer = function(container) {
+            Container.control({id: container.id, action: 'stop'}).$promise.then(function() {
+                $route.reload();
+            }, function(err) {
+                flash.error = 'error stopping container: ' + err.data;
+            });
+        };
+
+        vm.restartContainer = function(container) {
+            Container.control({id: container.id, action: 'restart'}).$promise.then(function() {
+                $route.reload();
+            }, function(err) {
+                flash.error = 'error restarting container: ' + err.data;
+            });
+        };
+        vm.showProgress = function() {
+            $('.ui.form').addClass('hide');
+            $('.progress').removeClass('hide');
+        };
+        vm.hideProgress = function() {
+            $('.ui.form').removeClass('hide');
+            $('.progress').addClass('hide');
+        };
+        vm.scaleContainer = function(container) {
+            vm.showProgress();
+            Container.control({id: container.id, action: 'scale', count: $scope.count}).$promise.then(function() {
+                $route.reload();
+            }, function(err) {
+                flash.error = 'error scaling container: ' + err.data;
+                vm.hideProgress();
+                $('.basic.modal').modal('hide');
+                $('.basic.modal').remove();
+            });
         }
     }
 
