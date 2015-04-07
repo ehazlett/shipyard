8e9c0c95c37a91a09fba2a0a8d9dea4783470832
Tom Barlow <tomwbarlow@gmail.com>
2015-04-07 18:51:06 +0100

    Operations button on container inspect page are now working
    Signed-off-by: Tom Barlow <tomwbarlow@gmail.com>

--- controller/static/app/containers/inspect.controller.js
+++ controller/static/app/containers/inspect.controller.js
@@ -5,9 +5,57 @@
 		.module('shipyard.containers')
 		.controller('ContainerController', ContainerController);
 
-	ContainerController.$inject = ['resolvedContainer'];
-	function ContainerController(resolvedContainer) {
+	ContainerController.$inject = ['resolvedContainer', 'ContainerService', '$state'];
+	function ContainerController(resolvedContainer, ContainerService, $state) {
         var vm = this;
         vm.container = resolvedContainer;
+        vm.showDestroyContainerDialog = showDestroyContainerDialog;
+        vm.showRestartContainerDialog = showRestartContainerDialog;
+        vm.showStopContainerDialog = showStopContainerDialog;
+        vm.destroyContainer = destroyContainer;
+        vm.stopContainer = stopContainer;
+        vm.restartContainer = restartContainer;
+
+        function showDestroyContainerDialog(container) {
+            vm.selectedContainerId = resolvedContainer.Id;
+            $('.ui.small.destroy.modal').modal('show');
+        }
+
+        function showRestartContainerDialog(container) {
+            vm.selectedContainerId = resolvedContainer.Id;
+            $('.ui.small.restart.modal').modal('show');
+        }
+
+        function showStopContainerDialog(container) {
+            vm.selectedContainerId = resolvedContainer.Id;
+            $('.ui.small.stop.modal').modal('show');
+        }
+
+        function destroyContainer() {
+            ContainerService.destroy(vm.selectedContainerId)
+                .then(function(data) {
+                    $state.transitionTo('dashboard.containers');
+                }, function(data) {
+                    vm.error = data;
+                });
+        }
+
+        function stopContainer() {
+            ContainerService.stop(vm.selectedContainerId)
+                .then(function(data) {
+                    $state.reload();
+                }, function(data) {
+                    vm.error = data;
+                });
+        }
+
+        function restartContainer() {
+            ContainerService.restart(vm.selectedContainerId)
+                .then(function(data) {
+                    $state.reload();
+                }, function(data) {
+                    vm.error = data;
+                });
+        }
 	}
 })();
