5d120893a48e80ff5a6051f3ec47685697c9b382
Tom Barlow <tomwbarlow@gmail.com>
2015-04-08 14:51:23 +0100

    Removed non-ASCII characters from logs
    Signed-off-by: Tom Barlow <tomwbarlow@gmail.com>

--- controller/static/app/containers/logs.controller.js
+++ controller/static/app/containers/logs.controller.js
@@ -8,6 +8,7 @@
 	LogsController.$inject = ['resolvedLogs'];
 	function LogsController(resolvedLogs) {
         var vm = this;
-        vm.logs = resolvedLogs;
+        // ASCII is in range of 0 to 127, so strip anything not in that range
+        vm.logs = resolvedLogs.replace(/[^\x00-\x7F]/g, "");
 	}
 })();
