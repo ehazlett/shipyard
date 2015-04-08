dacf33f53464ac40b632a9708466ccd2dd53c149
Tom Barlow <tomwbarlow@gmail.com>
2015-04-08 14:55:26 +0100

    Switched to Droid Sans Mono for fixed width font
    Signed-off-by: Tom Barlow <tomwbarlow@gmail.com>

--- controller/static/app/containers/logs.controller.js
+++ controller/static/app/containers/logs.controller.js
@@ -5,9 +5,10 @@
 		.module('shipyard.containers')
 		.controller('LogsController', LogsController);
 
-	LogsController.$inject = ['resolvedLogs'];
-	function LogsController(resolvedLogs) {
+	LogsController.$inject = ['resolvedLogs', '$stateParams'];
+	function LogsController(resolvedLogs, $stateParams) {
         var vm = this;
+        vm.id = $stateParams.id;
         // ASCII is in range of 0 to 127, so strip anything not in that range
         vm.logs = resolvedLogs.replace(/[^\x00-\x7F]/g, "");
 	}
