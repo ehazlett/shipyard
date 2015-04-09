1c874e536fac1702d395abcf633bf71c323d700b
Tom Barlow <tomwbarlow@gmail.com>
2015-04-09 14:11:52 +0100

    Deployment form is now able to add new rows for volumes, links, etc.
    Signed-off-by: Tom Barlow <tomwbarlow@gmail.com>

--- controller/static/app/containers/deploy.controller.js
+++ controller/static/app/containers/deploy.controller.js
@@ -9,13 +9,24 @@
     function ContainerDeployController($http, $state) {
         var vm = this;
         vm.cmd = "";
+        
+        vm.volumes = [];
+        vm.hostPath = "";
+        vm.containerPath = "";
+
+        vm.links = [];
+        vm.containerToLink = "";
+        vm.containerToLinkAlias = "";
+
         vm.ports = []; 
         vm.hostPort = "";
         vm.containerPort = "";
         vm.protocol = "";
+        
         vm.envVars = [];
         vm.variableName = "";
         vm.variableValue = "";
+        
         vm.deploying = false;
         vm.containerName = "";
         vm.error = "";
@@ -26,8 +37,38 @@
         };
 
         vm.deploy = deploy;
+        vm.pushVolume = pushVolume;
+        vm.deleteVolume = deleteVolume;
+        vm.pushLink = pushLink;
+        vm.removeLink = removeLink;
         vm.pushEnvVar = pushEnvVar;
+        vm.removeEnvVar = removeEnvVar;
         vm.pushPort = pushPort;
+        vm.removePort = removePort;
+
+        function pushVolume() {
+            var volume = {'HostPath': vm.hostPath, 'ContainerPath': vm.containerPath};
+            vm.volumes.push(volume);
+            vm.hostPath = "";
+            vm.containerPath = "";
+        }
+
+        function deleteVolume(volume) {
+            var index = vm.volumes.indexOf(volume);
+            vm.volumes.splice(index, 1);
+        }
+
+        function pushLink() {
+            var link = {'ContainerToLink': vm.containerToLink, 'ContainerToLinkAlias': vm.containerToLinkAlias};
+            vm.links.push(link);
+            vm.containerToLink = "";
+            vm.containerToLinkAlias = "";
+        }
+
+        function removeLink(link) {
+            var index = vm.links.indexOf(link);
+            vm.links.splice(index, 1);
+        }
 
         function pushPort() {
             var port = {'ContainerPort': vm.containerPort, 'HostIp': vm.hostIp, 'HostPort': vm.hostPort};
@@ -36,11 +77,21 @@
             vm.containerPort = "";
         }
 
+        function removePort(port) {
+            var index = vm.ports.indexOf(port);
+            vm.ports.splice(index, 1);
+        }
+
         function pushEnvVar() {
             var envVar = { name: vm.variableName, value: vm.variableValue };
             vm.envVars.push(envVar);
             vm.variableName = "";
             vm.variableValue = "";
+        }
+
+        function removeEnvVar(envVar) {
+            var index = vm.envVars.indexOf(envVar);
+            vm.envVars.splice(index, 1);
         }
 
         function transformEnvVars() {
