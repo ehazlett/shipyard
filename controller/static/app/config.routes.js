be402ca08985f325844da0186acf3a688c184b72
Tom Barlow <tomwbarlow@gmail.com>
2015-04-14 12:52:09 +0100

    Make the error page a little prettier
    Signed-off-by: Tom Barlow <tomwbarlow@gmail.com>

--- controller/static/app/config.routes.js
+++ controller/static/app/config.routes.js
@@ -10,7 +10,6 @@
     function getRoutes($stateProvider, $urlRouterProvider) {	
         $stateProvider
             .state('error', {
-                url: '/error',
                 templateUrl: 'app/error/error.html',
                 authenticate: false
             });
