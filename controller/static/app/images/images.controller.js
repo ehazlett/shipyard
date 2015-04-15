e67e1aaa96fa420de1b58ae0a8db5b3c8d839d2a
Evan Hazlett <ejhazlett@gmail.com>
2015-04-15 01:17:06 -0400

    images: add ability to pull images
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- controller/static/app/images/images.controller.js
+++ controller/static/app/images/images.controller.js
@@ -9,19 +9,28 @@
 	function ImagesController(images, ImagesService, $state, $timeout) {
             var vm = this;
             vm.images = images;
+            vm.pulling = false;
             vm.selectedImage = null;
             vm.refresh = refresh;
             vm.removeImage = removeImage;
+            vm.pullImage = pullImage;
+            vm.pullImageName = "";
             vm.showRemoveImageDialog = showRemoveImageDialog;
+            vm.showPullImageDialog = showPullImageDialog;
 
             function showRemoveImageDialog(image) {
                 vm.selectedImage = image;
                 $('.ui.small.remove.modal').modal('show');
             }
 
+            function showPullImageDialog(image) {
+                $('.ui.small.pull.modal').modal('show');
+            }
+
             function refresh() {
                 ImagesService.list()
                     .then(function(data) {
+                        console.log(data.length);
                         vm.images = data; 
                     }, function(data) {
                         vm.error = data;
@@ -37,6 +46,24 @@
                         vm.error = data;
                     });
                 vm.error = "";
+            }
+
+            function pullImage() {
+                vm.pulling = true;
+                // this is to prevent errors in the console since we
+                // get a stream like response back
+                oboe({
+                    url: '/images/create?fromImage=' + vm.pullImageName,
+                    method: "POST"
+                })
+                .done(function() {
+                    vm.refresh();
+                    vm.pulling = false;
+                    vm.pullImageName = "";
+                })
+                .fail(function(err) {
+                    vm.error = err;
+                });
             }
 	}
 })();
