e8f37dcab00a777c97d14248a229dc7544facc6e
Evan Hazlett <ejhazlett@gmail.com>
2015-04-21 17:21:02 -0400

    fix 401 and 403 redirects
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- controller/static/app/shipyard.jwt.js
+++ controller/static/app/shipyard.jwt.js
@@ -25,42 +25,36 @@
                 }
                 return $q.reject(response);
             };
-        })
-    .service('401interceptor', function($rootScope) {
-        var service = this;
-        service.responseError = function(response) {
-            if(response.status === 401) {
-                $rootScope.$state.go('login');
-            }
-            return response;
-        };
-    })
-    .service('403interceptor', function($rootScope) {
-        var service = this;
-        service.responseError = function(response) {
-            if(response.status === 403) {
-                $rootScope.$state.go('403');
-            }
-            return response;
-        };
-    })
-    .config([
-            '$httpProvider',
-            'jwtInterceptorProvider', 
-            function ($httpProvider, jwtInterceptorProvider) {
-                jwtInterceptorProvider.tokenGetter = function() {
-                    // Temporary solution to get angular-jwt to use our header format
-                    jwtInterceptorProvider.authHeader = "X-Access-Token";
-                    jwtInterceptorProvider.authPrefix = "";
-
-                    return localStorage.getItem('X-Access-Token');
+            })
+            .service('401interceptor', function($rootScope) {
+                var service = this;
+                service.responseError = function(response) {
+                    if(response.status === 401) {
+                        console.log("401");
+                        $rootScope.$state.go('login');
+                    }
+                    if(response.status === 403) {
+                        $rootScope.$state.go('403');
+                    }
+                    return response;
                 };
+            })
+            .config([
+                    '$httpProvider',
+                    'jwtInterceptorProvider', 
+                    function ($httpProvider, jwtInterceptorProvider) {
+                        jwtInterceptorProvider.tokenGetter = function() {
+                            // Temporary solution to get angular-jwt to use our header format
+                            jwtInterceptorProvider.authHeader = "X-Access-Token";
+                            jwtInterceptorProvider.authPrefix = "";
 
-                $httpProvider.interceptors.push('jwtInterceptor');
-                $httpProvider.interceptors.push('spinnerInterceptor');
-                $httpProvider.interceptors.push('401interceptor');
-                $httpProvider.interceptors.push('403interceptor');
-            }
-    ]);
+                            return localStorage.getItem('X-Access-Token');
+                        };
+
+                        $httpProvider.interceptors.push('jwtInterceptor');
+                        $httpProvider.interceptors.push('spinnerInterceptor');
+                        $httpProvider.interceptors.push('401interceptor');
+                    }
+            ]);
 })();
 
