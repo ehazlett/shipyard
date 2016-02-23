d7619183c9ef3040120da910c40395364d3ee714
Jonathan Rosado <jonathan.rosado-lugo@hpe.com>
2016-02-23 15:37:45 -0500

    - Added image edit view

--- controller/static/app/projects/edit.controller.js
+++ controller/static/app/projects/edit.controller.js
@@ -10,13 +10,34 @@
         var vm = this;
 
         vm.project = resolvedProject;
-        vm.skipImages = true;
-        vm.skipTests = true;
+
+        //TODO: Should the default state be on or off (i.e. checked or not checked)?
+        vm.skipImages = false;
+        vm.skipTests = false;
 
         vm.imageList = imageList;
+        vm.showScaleContainerDialog = showScaleContainerDialog;
+        vm.scaleContainer = scaleContainer;
 
         function imageList() {
             return Object.keys(vm.project.image_list)
+        }
+
+        function showScaleContainerDialog(id) {
+            vm.selectedImageId = id;
+            $('#scale-modal').modal('show');
+        }
+
+        function scaleContainer() {
+            ContainerService.scale(vm.selectedContainerId, vm.numOfInstances)
+                .then(function(response) {
+                    vm.refresh();
+                }, function(response) {
+                    // Add unique errors to vm.errors
+                    $.each(response.data.Errors, function(i, el){
+                        if($.inArray(el, vm.errors) === -1) vm.errors.push(el);
+                    });
+                });
         }
     }
 })();
