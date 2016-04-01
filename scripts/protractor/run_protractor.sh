b7ab4d58f435a4eb22e97d391298f2684e4038ad
Ricardo Quintana <ricardo.quintana@hpe.com>
2016-04-01 17:52:41 -0400

    - Removed shipyard.iml file (that's IDEA config file, should not be there) - Moved protractor scripts to scripts.

--- scripts/protractor/run_protractor.sh
+++ scripts/protractor/run_protractor.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
 DOCKER_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
 
+# TODO: need to evaluate this image with headless chrome in CI environment
 sudo docker run -it --privileged -e DOCKER_HOST=$DOCKER_HOST --rm --net=host -v /dev/shm:/dev/shm -v $(pwd):/protractor webnicer/protractor-headless conf.js
