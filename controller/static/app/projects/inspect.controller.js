81873de5d415d463750ee1c0b0ebe3963998fef3
Andreea <andreea-dora.garda@hpe.com>
2016-05-10 11:34:04 +0300

    enable inspect action button only if we have test results

--- controller/static/app/projects/inspect.controller.js
+++ controller/static/app/projects/inspect.controller.js
@@ -10,13 +10,28 @@
         var vm = this;
 
         vm.showProjectHistory = showProjectHistory;
+        vm.testResults = testResults;
 
         vm.results = resolvedResults;
+        angular.forEach(vm.results.testResults, function (result, key) {
+            testResults(vm.results.projectId,result.testId,result.buildId).then(function (response) {
+                vm.results[key].istestResult = response;
+            })
+        });
 
         function showProjectHistory() {
             $('#inspect-project-history-' + vm.results.projectId)
                 .sidebar('toggle')
             ;
+        }
+
+        function testResults(projectId, testId, buildId) {
+            return ProjectService.buildResults(projectId, testId, buildId)
+                .then(function(data) {
+                    return true;
+                }, function(data) {
+                    return false;
+                });
         }
     }
 })();
