3af28606e572b3591d798281352a7113110e0e86
tomwbarlow@gmail.com <tomwbarlow@gmail.com>
2016-06-30 07:04:00 +0100

    Make the login form a little less ugly
    Signed-off-by: tomwbarlow@gmail.com <tomwbarlow@gmail.com>

--- controller/static/client/components/login/LoginView.js
+++ controller/static/client/components/login/LoginView.js
@@ -8,38 +8,41 @@ const LoginView = React.createClass({
   },
   render() {
     return (
-      <div className="ui middle aligned center aligned grid">
-        <div className="column">
-          <h2 className="ui teal image header">
-            <div className="content">
-              Log-in to your account
-            </div>
-          </h2>
-          <form className="ui large form">
-            <div className="ui stacked segment">
-              <div className="field">
-                <div className="ui left icon input">
-                  <i className="user icon"></i>
-                  <input type="text" name="username" ref="username" placeholder="Username"></input>
+      <div style={{ margin: '0 auto', marginTop: '5em', width: '400px' }}>
+        <div className="ui middle aligned center aligned grid">
+          <div className="column">
+            <h2 className="ui blue image header">
+              <div className="content">
+                Shipyard
+              </div>
+            </h2>
+            <form className="ui large form">
+              <div className="ui segment">
+                <div className="field">
+                  <div className="ui left icon input">
+                    <i className="user icon"></i>
+                    <input type="text" name="username" ref="username" placeholder="Username">
+                    </input>
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="ui left icon input">
+                    <i className="lock icon"></i>
+                    <input type="password" name="password" ref="password" placeholder="Password">
+                    </input>
+                  </div>
+                </div>
+                <div className="ui fluid large blue submit button" onClick={this.tryLogin}>
+                  Login
                 </div>
               </div>
-              <div className="field">
-                <div className="ui left icon input">
-                  <i className="lock icon"></i>
-                  <input type="password" name="password" ref="password" placeholder="Password"></input>
-                </div>
-              </div>
-              <div className="ui fluid large teal submit button" onClick={this.tryLogin}>Login</div>
-            </div>
-
-            <div className="ui error message"></div>
-
-          </form>
-
+              <div className="ui error message"></div>
+            </form>
+          </div>
         </div>
       </div>
     );
-  }
+  },
 });
 
 export default LoginView;
