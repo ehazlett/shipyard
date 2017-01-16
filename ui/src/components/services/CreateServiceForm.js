d9cdfac3fc31e6343435d2d6dfb8ac00fda157b6
Tyler Cloutier <cloutiertyler@aol.com>
2017-01-15 18:48:23 -0800

    Added the ability to create a service with Mounts.

--- ui/src/components/services/CreateServiceForm.js
+++ ui/src/components/services/CreateServiceForm.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { Message, Header, Divider } from 'semantic-ui-react';
-import { Redirect } from 'react-router';
+import { Message, Header, Divider, List, Button } from "semantic-ui-react";
+import { Redirect } from "react-router";
 
-import Form from '../common/Form';
-import { createService, dockerErrorHandler } from '../../api';
+import Form from "../common/Form";
+import { createService, dockerErrorHandler } from "../../api";
 
 const VALIDATION_CONFIG = {
   Image:{
-		identifier: 'Image',
-		rules: [{
-			type: 'empty',
-		}],
-	},
+    identifier: "Image",
+    rules: [{
+      type: "empty",
+    }],
+  },
   Mode: {
-		identifier: 'Mode',
-		rules: [{
-			type: 'empty',
-			prompt: 'Please select a scheduler mode',
-		}],
-	},
+    identifier: "Mode",
+    rules: [{
+      type: "empty",
+      prompt: "Please select a scheduler mode",
+    }],
+  },
 };
 
 export default class CreateServiceForm extends React.Component {
@@ -27,25 +27,71 @@ export default class CreateServiceForm extends React.Component {
     error: null,
     loading: false,
     redirect: false,
-    redirectTo: '',
-		validationConfig: VALIDATION_CONFIG,
+    redirectTo: "",
+    validationConfig: VALIDATION_CONFIG,
+    mountInputKeys: [],
+    mountsAdded: 0
   };
+
+  getMountsFromFormValues = (values) => {
+    const filterKeys = (obj, predicate) => {
+      return Object.keys(obj)
+            .filter( key => predicate(key) )
+            .reduce( (res, key) => {res[key] = obj[key]; return res}, {});
+    };
+    const dynamicFormElements = filterKeys(values, key => key.includes("-"));
+    const splitDynamicFormElements = Object.keys(dynamicFormElements).map(key => {
+      const split = key.split("-");
+      return [...split, dynamicFormElements[key]];
+    });
+
+    // Groups mount fields by the row that they are in, and creates an object
+    // out of each field.
+    const mountsObject = splitDynamicFormElements.reduce((acc, tuple) => {
+      const rowTag = tuple[1];
+      const key = tuple[0];
+      const value = tuple[2];
+      const output = {...acc};
+      if (output[rowTag] === undefined) {
+        output[rowTag] = {};
+      }
+      output[rowTag][key] = value;
+      return output;
+    }, {});
+
+    const mounts = Object.keys(mountsObject).map(key => mountsObject[key]);
+    return mounts.map(mount => {
+      return {
+        "Target": mount["Target"],
+        "Source": mount["Source"],
+        "Type": mount["MountType"],
+        "ReadOnly": mount["ReadOnly"] === "true",
+        "VolumeOptions": {
+          "DriverConfig": {
+            "Name": mount["VolumeDriver"]
+          },
+        },
+      };
+    });
+  }
 
   createService = (e, values) => {
     e.preventDefault();
-
+    const mounts = this.getMountsFromFormValues(values);
+    console.log(mounts);
     createService({
       Name: values.Name,
       TaskTemplate: {
         ContainerSpec: {
           Image: values.Image,
-          Args: values.Args ? values.Args.split(' ') : null,
-          Command: values.Command ? values.Command.split(' ') : null,
+          Args: values.Args ? values.Args.split(" ") : null,
+          Command: values.Command ? values.Command.split(" ") : null,
           User: values.User ? values.User : null,
           Dir: values.Dir ? values.Dir : null,
-          Groups: values.Command ? values.Command.split(' ') : null,
-					TTY: values.TTY,
-					OpenStdin: values.OpenStdin,
+          Groups: values.Command ? values.Command.split(" ") : null,
+          TTY: values.TTY,
+          OpenStdin: values.OpenStdin,
+          Mounts: mounts,
         },
       },
       Mode: {
@@ -63,92 +109,135 @@ export default class CreateServiceForm extends React.Component {
         });
       })
       .catch((error) => {
-				dockerErrorHandler(error.response)
-					.then((parsedError) => {
-						this.setState({
-							error: parsedError.desc,
-							loading: false
-						});
-					});
+        dockerErrorHandler(error.response)
+          .then((parsedError) => {
+            this.setState({
+              error: parsedError.desc,
+              loading: false
+            });
+          });
       });
   }
 
-	updateValidationConfig = (key, value) => {
-		const validationConfig = Object.assign({}, this.state.validationConfig);
+  updateValidationConfig = (key, value) => {
+    const validationConfig = Object.assign({}, this.state.validationConfig);
 
-		if(key === 'Mode' && value === 'Replicated') {
-			validationConfig.Replicas = {
-				identifier: 'Replicas',
-				rules: [{
-					type: 'empty',
-				}],
-			};
-		} else {
-			delete validationConfig.Replicas;
-		}
+    if(key === "Mode" && value === "Replicated") {
+      validationConfig.Replicas = {
+        identifier: "Replicas",
+        rules: [{
+          type: "empty",
+        }],
+      };
+    } else {
+      delete validationConfig.Replicas;
+    }
 
-		this.setState({
-			validationConfig,
-		});
-	};
+    this.setState({
+      validationConfig,
+    });
+  };
 
+  handleChange = (key, e) => {
+    this.updateValidationConfig(key, e.target.value);
+    this.setState({
+      [key]: e.target.value,
+    });
+  }
 
-	handleChange = (key, e) => {
-		this.updateValidationConfig(key, e.target.value);
-		this.setState({
-			[key]: e.target.value,
-		});
-	}
+  addMount = () => {
+    this.setState({
+      ...this.state,
+      mountsAdded: this.state.mountsAdded + 1,
+      mountInputKeys: [...this.state.mountInputKeys, this.state.mountsAdded]
+    })
+  }
+
+  removeMount = (e, button) => {
+    console.log(e, button)
+    this.setState({
+      mountInputKeys: this.state.mountInputKeys.filter((_, i) => i !== button.tabIndex )
+    });
+  }
 
   render() {
     const { redirect, redirectTo, Mode, error } = this.state;
+    const mountTypes = [
+      { text: "Volume", value: "volume" },
+      { text: "Bind", value: "bind" },
+    ];
+    const readOnlyOptions = [
+      { text: "Read Only", value: "true" },
+      { text: "Read/Write", value: "false" },
+    ];
     return (
       <Form inline fields={this.state.validationConfig} onSubmit={this.createService}>
         {redirect && <Redirect to={redirectTo}/>}
-				{error && <Message negative>{error}</Message>}
+        {error && <Message negative>{error}</Message>}
         <Header>Create a Service</Header>
-				<Form.Group widths="equal">
-					<Form.Input name="Image" label="Image" placeholder="dockercloud/hello-world" required />
-					<Form.Input name="Name" label="Service Name" placeholder="hello-world" />
-				</Form.Group>
-
-				<Divider hidden />
-
-				<Form.Group widths="equal">
-					<Form.Input name="Command" label="Command" placeholder="Default" />
-					<Form.Input name="Args" label="Args" placeholder="Default" />
-				</Form.Group>
-
-				<Divider hidden />
-
         <Form.Group widths="equal">
-					<Form.Field name="Mode" label="Mode" onChange={(e) => this.handleChange('Mode', e)} control="select" required>
-						<option value=""></option>
-						<option value="Replicated">Replicated</option>
-						<option value="Global">Global</option>
-					</Form.Field>
-          <Form.Input name="Replicas" label="Replicas" type="number" disabled={Mode !== 'Replicated'} required={Mode === 'Replicated'} placeholder={1} />
+          <Form.Input name="Image" label="Image" placeholder="dockercloud/hello-world" required />
+          <Form.Input name="Name" label="Service Name" placeholder="hello-world" />
         </Form.Group>
 
-				<Divider hidden />
+        <Divider hidden />
 
-				<Form.Group widths="equal">
-					<Form.Input name="User" label="User" placeholder="nobody" />
-					<Form.Input name="Dir" label="Working Directory" placeholder="/usr/src/app" />
-					<Form.Input name="Groups" label="Groups" placeholder="group1 group2" />
-				</Form.Group>
+        <Form.Group widths="equal">
+          <Form.Input name="Command" label="Command" placeholder="Default" />
+          <Form.Input name="Args" label="Args" placeholder="Default" />
+        </Form.Group>
 
-				{/* These don't seem to do anything at the moment 
-				<Form.Field>
-					<label>Stream Options</label>
-					<Form.Group inline>
-						<Form.Checkbox label='Attach TTY' name='TTY' />
-						<Form.Checkbox label='Open stdin' name='OpenStdin' />
-					</Form.Group>
-				</Form.Field>
-				*/}
+        <Divider hidden />
 
-				<Divider hidden />
+        <Form.Group widths="equal">
+          <Form.Field name="Mode" label="Mode" onChange={(e) => this.handleChange("Mode", e)} control="select" required>
+            <option value=""></option>
+            <option value="Replicated">Replicated</option>
+            <option value="Global">Global</option>
+          </Form.Field>
+          <Form.Input name="Replicas" label="Replicas" type="number" disabled={Mode !== "Replicated"} required={Mode === "Replicated"} placeholder={1} />
+        </Form.Group>
+
+        <Divider hidden />
+
+        <Form.Group widths="equal">
+          <Form.Input name="User" label="User" placeholder="nobody" />
+          <Form.Input name="Dir" label="Working Directory" placeholder="/usr/src/app" />
+          <Form.Input name="Groups" label="Groups" placeholder="group1 group2" />
+        </Form.Group>
+
+        <Divider hidden />
+
+        <Header as="h4">Mounts</Header>
+        <List>
+          {this.state.mountInputKeys.map((key, i) => {
+            return (
+              <List.Item key={key}>
+                <Form.Group widths="equal">
+                  <Form.Select name={"MountType-" + i} label={i === 0 && "Type"} options={mountTypes} placeholder="Type" />
+                  <Form.Input name={"Source-" + i} label={i === 0 && "Source"} placeholder="volumename or /host/path" />
+                  <Form.Input name={"Target-" + i} label={i === 0 && "Target"} placeholder="/container/path" />
+                  <Form.Input name={"VolumeDriver-" + i} label={i === 0 && "Volume Driver"} placeholder="local" />
+                  <Form.Select name={"ReadOnly-" + i} label={i === 0 && "Read Only"} options={readOnlyOptions} placeholder="Read Only" />
+                  <Form.Button type="button" tabIndex={i} width={4} icon="minus" label={i === 0 && "Remove"} basic onClick={this.removeMount}/>
+                </Form.Group>
+              </List.Item>
+            );
+          })}
+        </List>
+        <Button type="button" icon="plus" basic onClick={this.addMount}/>
+
+        {/* These don"t seem to do anything at the moment
+        <Form.Field>
+          <label>Stream Options</label>
+          <Form.Group inline>
+            <Form.Checkbox label="Attach TTY" name="TTY" />
+            <Form.Checkbox label="Open stdin" name="OpenStdin" />
+          </Form.Group>
+        </Form.Field>
+        */}
+
+        <Divider hidden />
         <Form.Button color="green">Create Service</Form.Button>
       </Form>
     );
