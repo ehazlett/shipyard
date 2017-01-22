d899e524d4bf858a645bf6e24bffd3599d210df7
Tom Barlow <tomwbarlow@gmail.com>
2017-01-22 15:32:02 +0000

    Added login form validation and split into form and view components
    Signed-off-by: Tom Barlow <tomwbarlow@gmail.com>

--- ui/src/components/login/LoginView.js
+++ ui/src/components/login/LoginView.js
@@ -1,47 +1,15 @@
 import React from 'react';
 
 import { Redirect } from 'react-router';
-import { Divider, Grid, Form, Header, Message } from 'semantic-ui-react';
+import { Divider, Grid, Header } from 'semantic-ui-react';
 
-import { login } from '../../api';
+import LoginForm from "./LoginForm";
 import { getAuthToken } from '../../services/auth';
 
 export default class LoginView extends React.Component {
   state = {
     error: null,
-    loading: false,
-    redirect: false
-  };
-
-  handleLoginSuccess = () => {
-    this.setState({
-      error: null,
-      loading: false,
-      redirect: true,
-    });
-  };
-
-  handleLoginError = (error) => {
-    error.response.text()
-      .then((text) => {
-        this.setState({
-          error: text,
-          loading: false,
-        });
-      });
-  };
-
-  tryLogin = (e, values) => {
-    this.setState({
-      error: null,
-      loading: true,
-    });
-
-    login(values.formData.username, values.formData.password)
-      .then(this.handleLoginSuccess)
-      .catch(this.handleLoginError);
-
-    e.preventDefault();
+    redirect: false,
   };
 
   componentDidMount() {
@@ -52,8 +20,15 @@ export default class LoginView extends React.Component {
     }
   }
 
+  handleLoginSuccess = () => {
+    console.log('Handling success');
+    this.setState({
+      redirect: true,
+    });
+  };
+
   render() {
-    const { redirect, error } = this.state;
+    const { redirect } = this.state;
     return (
         <Grid verticalAlign="middle" centered style={{ height: '100vh', width: '100vw', backgroundColor: '#2185D0', padding: 0, margin: 0 }}>
           {redirect && (
@@ -65,13 +40,7 @@ export default class LoginView extends React.Component {
                 Shipyard
               </Header>
               <Divider hidden />
-              <Form onSubmit={this.tryLogin}>
-                {error && (<Message negative>{this.state.error}</Message>)}
-                <Message error />
-                <Form.Input name="username" icon="user" iconPosition="left" placeholder="Username" autoFocus />
-                <Form.Input name="password" icon="lock" iconPosition="left" placeholder="Password" type="password" />
-                <Form.Button fluid inverted basic type="submit">Login</Form.Button>
-              </Form>
+              <LoginForm successHandler={this.handleLoginSuccess} />
             </Grid.Column>
           </Grid.Row>
         </Grid>
