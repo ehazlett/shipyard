7ed949ba6c1403d49358ade030df12342b8070a1
Tom Barlow <tomwbarlow@gmail.com>
2017-01-24 10:11:03 +0000

    Fix padding on create service/volume forms
    Signed-off-by: Tom Barlow <tomwbarlow@gmail.com>

--- ui/src/components/services/CreateServiceView.js
+++ ui/src/components/services/CreateServiceView.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { Segment } from 'semantic-ui-react';
+
 import CreateServiceForm from './CreateServiceForm';
 
 const CreateServiceView = (props) => (
-  <CreateServiceForm {...props} />
+  <Segment basic>
+    <CreateServiceForm {...props} />
+  </Segment>
 );
 
 export default CreateServiceView;
